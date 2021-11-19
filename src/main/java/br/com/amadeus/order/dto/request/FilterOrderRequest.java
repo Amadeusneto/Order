072a76def9649a2305e11a89d3aa1c532cd8f16f
Amadeusneto <amadeusmoura@gmail.com>
2021-11-19 10:51:55 -0300

    Ajuste no campo data

--- src/main/java/br/com/amadeus/order/dto/request/FilterOrderRequest.java
+++ src/main/java/br/com/amadeus/order/dto/request/FilterOrderRequest.java
@@ -1,5 +1,6 @@
 package br.com.amadeus.order.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,5 +8,6 @@ import java.time.LocalDate;
 @Data
 public class FilterOrderRequest {
     private Long clientCode;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate registrationDate;
 }
