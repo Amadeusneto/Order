13690310bb8fbcbeedf6bc7cc39ea4630668c7fb
Amadeusneto <amadeusmoura@gmail.com>
2021-11-19 10:46:26 -0300

    Ajuste no campo data

--- src/main/java/br/com/amadeus/order/dto/response/OrderResponse.java
+++ src/main/java/br/com/amadeus/order/dto/response/OrderResponse.java
@@ -1,5 +1,6 @@
 package br.com.amadeus.order.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 @Data
 public class OrderResponse {
     private Long controlNumber;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate registrationDate;
     private Integer quantity;
     private Long clientCode;
