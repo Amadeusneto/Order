29f24497c301d97feaf208e6663b57add5369398
Amadeusneto <amadeusmoura@gmail.com>
2021-11-18 22:28:19 -0300

    Ajuste no dto

--- src/main/java/br/com/amadeus/order/dto/request/OrderRequest.java
+++ src/main/java/br/com/amadeus/order/dto/request/OrderRequest.java
@@ -1,12 +1,16 @@
 package br.com.amadeus.order.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
 
     private Long controlNumber;
